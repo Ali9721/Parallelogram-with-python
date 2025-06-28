b0ff214597219e75a97d0e873685877d00646b7c
Ali Najafi <76534541+Ali9721@users.noreply.github.com>
2025-06-28 09:46:52 -0700

    Parallelogram-with-python
     I write simple program with python for this formula of math  =>  S = a (height) * b (weight) .

---
+++ Parallelogram.py
@@ -0,0 +1,7 @@
+weight = float(input("Enter the length of the weight: "))
+ 
+height = float(input("Enter the length of the height: "))
+
+# Now I use formula of math for this simple project
+area = weight * height
+print(f"The area of the parallelogram is: {area}")
